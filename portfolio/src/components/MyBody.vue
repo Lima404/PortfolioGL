e81a690ca52b9693f2b34e193879b8506ac4e321
gabriel <gabriel.lima2307@uol.com.br>
2023-05-03 17:38:09 -0300

    create file body for adm others components

---
+++ portfolio/src/components/MyBody.vue
@@ -0,0 +1,30 @@
+<template lang="pt-br">
+    <div class="body">
+        <MyTitle></MyTitle>
+        <MyHero></MyHero>
+        <MyProjects></MyProjects>
+    </div>
+</template>
+<script>
+import MyTitle from '@/components/home/MyTitle.vue'
+import MyHero from '@/components/home/MyHero.vue'
+import MyProjects from '@/components/home/MyProjects.vue'
+export default {
+
+    components: { MyTitle , MyHero , MyProjects }
+    
+}
+</script>
+<style scoped>
+
+
+    .body{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 0px;
+        gap: 30px;
+    }
+    
+    
+</style>
